19f23015b5883b213ed991c8f909a42d705a1275
Wels Theory <williams.limonchi.sandoval@gmail.com>
2024-12-20 21:23:25 -0500

    Clase PWM y Clase Timer

---
+++ Timer/Core/Inc/app.h
@@ -0,0 +1,22 @@
+/*
+ * app.h
+ *
+ *  Created on: Dec 6, 2024
+ *      Author: Wels
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "main.h"
+
+void app_main(void);
+
+#endif /* INC_APP_H_ */
+
+
+
+
+
+
+
---
+++ Timer/Core/Src/app.c
@@ -0,0 +1,21 @@
+/*
+ * app.c
+ *
+ *  Created on: Dec 6, 2024
+ *      Author: Wels
+ */
+#include "app.h"
+
+void app_main(void)
+{
+	while(1)
+	{
+		asdasd
+		asdas
+	}
+}
+
+
+
+
+
